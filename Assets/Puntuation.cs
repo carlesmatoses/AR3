630ee17d622c994ec4d4a18d92f4bb5098d1724e
carlesmatoses <carlesmatosesg@gmail.com>
2024-12-16 16:05:45 +0100

    goals color, puntuation text

--- Assets/Puntuation.cs
+++ Assets/Puntuation.cs
@@ -1,14 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Puntuation : MonoBehaviour
 {
     float _puntuation=0;
 
+    public TMP_Text puntuationText;
+
+    void Update()
+    {
+    }
+
     public void IncreasePuntuation(float x)
     {
         _puntuation += x;
+        puntuationText.text = _puntuation.ToString();
+
     }
 
 }
